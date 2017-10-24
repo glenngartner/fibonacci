f932567b951f4d6beed663d0a73a5999ba256f2a
Glenn Gartner <glenngartner@gmail.com>
2017-10-23 21:23:31 -0400

    update .gitignore to exclude dev directories

---
+++ cmake-build-debug/CMakeFiles/fibonacci.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fibonacci.dir/src/main.cpp.obj"
+  "CMakeFiles/fibonacci.dir/src/Fibonacci.cpp.obj"
+  "fibonacci.pdb"
+  "fibonacci.exe"
+  "fibonacci.exe.manifest"
+  "libfibonacci.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fibonacci.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
